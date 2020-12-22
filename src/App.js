1dd447c9fad521c8b66ab8fbda544e418fbbd812
Gawel <gawel@pluxcustoms.nl>
2020-12-22 14:49:37 +0100

    podział na componenty

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './App.css';
-import './homepage.styles.scss';
+import './pages/homepage.styles.scss';
 
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage.component.jsx";
 
 function App() {
   return (
