f5faab522441ed8f20165e764c8c6854c2e19daa
Gawel <gawel@pluxcustoms.nl>
2020-12-23 01:10:24 +0100

    update menuitem to navigate with router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import './pages/homepage.styles.scss';
 
 import HomePage from "./pages/homepage.component.jsx";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+
+)
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
