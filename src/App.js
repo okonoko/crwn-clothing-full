5f80db6fe2924a1f4bf5d44d4e1c1fc6c4e1d024
Gawel <gawel@pluxcustoms.nl>
2021-02-08 14:41:05 +0100

    adding sign in functionality

--- src/App.js
+++ src/App.js
@@ -8,19 +8,44 @@ import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx'
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component.jsx'
+import {auth} from './firebase/firebase.utils'
 
+// import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInSignUp}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInSignUp}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -35,7 +35,8 @@ class SignIn extends React.Component {
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput 
-                        name='email' 
+                        name='email'
+                        type='email'
                         value={this.state.email} 
                         handleChange={this.handleChange}
                         label='email'
@@ -44,14 +45,16 @@ class SignIn extends React.Component {
 
                     <FormInput 
                         name='password'
+                        type='password'
                         value={this.state.password}
                         handleChange={this.handleChange}
                         label='password'
                         required
                     />
-
-                    <CustomButton type='submit'>Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                    </div>
                 </form>
             </div>
         )
